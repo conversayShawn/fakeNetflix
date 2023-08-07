2da9d8b30b7dbdd24cbe961da50760a067529198
conversayShawn <shawn.harris3510@gmail.com>
2023-08-07 08:10:23 -0700

    add login page

---
+++ src/components/main-page/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <form className="login-form-container">
+      <input type="text" placeholder="username"></input>
+      <input type="password" placeholder="password"></input>
+    </form>
+  );
+};
+
+export default Login;
