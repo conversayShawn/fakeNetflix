243ec89308ddc46034d5ebe25f931313d9b8e4d6
conversayShawn <shawn.harris3510@gmail.com>
2023-08-07 15:19:36 -0700

    finally works as intended

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Home from './components/main-page/Home';
+// import Login from './components/main-page/Login';
+// import CartoonList from './components/cartoons/CartoonList';
+// import MovieList from './components/movies/MovieList';
+// import Navigation from './components/main-page/Navigation';
+
+// const App = () => {
+//   // Use state to manage the authentication status
+//   const [authenticated, setAuthenticated] = useState(false);
+//   const [username, setUsername] = useState('');
+
+//   const handleLogin = (user) => {
+//     setUsername(user);
+//     setAuthenticated(true);
+//   };
+
+//   const handleLogout = () => {
+//     setAuthenticated(false);
+//   };
+
+//   return (
+//     <Router>
+//       <div className="app">
+//         <h1>Childhood Memories App</h1>
+//         <Navigation authenticated={authenticated} username={username} onLogout={handleLogout} />
+//         <hr />
+//         <Routes>
+//           {/* Render Home or Navigate to /login based on authentication */}
+//           <Route path="/" element={authenticated ? <Home /> : <Navigate to="/login" />} />
+//           <Route path="/login" element={<Login onLogin={handleLogin} />} />
+//           <Route path="/cartoons" element={<CartoonList />} />
+//           <Route path="/movies" element={<MovieList />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/main-page/Home';
 import Login from './components/main-page/Login';
 import CartoonList from './components/cartoons/CartoonList';
 import MovieList from './components/movies/MovieList';
 import Navigation from './components/main-page/Navigation';
+import axios from 'axios';
 
 const App = () => {
   // Use state to manage the authentication status
   const [authenticated, setAuthenticated] = useState(false);
   const [username, setUsername] = useState('');
+  const [cartoons, setCartoons] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const handleLogin = (user) => {
     setUsername(user);
@@ -19,6 +64,18 @@ const App = () => {
   const handleLogout = () => {
     setAuthenticated(false);
   };
+
+  useEffect(() => {
+    // Fetch cartoons data
+    axios.get('/data/cartoons.json').then((response) => {
+      setCartoons(response.data);
+    });
+
+    // Fetch movies data
+    axios.get('/data/movies.json').then((response) => {
+      setMovies(response.data);
+    });
+  }, []);
 
   return (
     <Router>
@@ -30,8 +87,8 @@ const App = () => {
           {/* Render Home or Navigate to /login based on authentication */}
           <Route path="/" element={authenticated ? <Home /> : <Navigate to="/login" />} />
           <Route path="/login" element={<Login onLogin={handleLogin} />} />
-          <Route path="/cartoons" element={<CartoonList />} />
-          <Route path="/movies" element={<MovieList />} />
+          <Route path="/cartoons" element={<CartoonList cartoons={cartoons} />} />
+          <Route path="/movies" element={<MovieList movies={movies} />} />
         </Routes>
       </div>
     </Router>
