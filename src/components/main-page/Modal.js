ba3d1e9aedad4cca24924d40510944b5f9fc1f11
conversayShawn <shawn.harris3510@gmail.com>
2023-08-07 08:24:28 -0700

    add modal to display when movie or cartoon show is clicked

---
+++ src/components/main-page/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div>
+      <h1>Sorry! This is a fake app. </h1>
+      <img></img>
+    </div>
+  );
+};
+
+export default Modal;
