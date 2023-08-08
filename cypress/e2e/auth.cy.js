b135f8a983f4eac214c4c73d1269512dcb0f33b3
conversayShawn <shawn.harris3510@gmail.com>
2023-08-08 06:32:30 -0700

    adding e2e test

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,17 @@
+describe('authentication process', () => {
+  it('verifies the user is logged in', () => {
+    // Visit our URL
+    cy.visit('http://localhost:3000/login')
+
+    // Verify user is not logged in
+    cy.get('.logout-btn').should('not.exist')
+
+    // Fill out & submit the form
+    cy.get('#username').eq(0).type('username@gmail.com')
+    cy.get('#password').eq(0).type('password123')
+    cy.get('.login-btn').click()
+
+    // Verify user is logged in
+    cy.get('.logout-btn').should('exist')
+  })
+})
