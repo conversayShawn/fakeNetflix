31e587b85b4f7e3e0eb5296b8cb545122239fb90
conversayShawn <shawn.harris3510@gmail.com>
2023-08-08 06:31:19 -0700

    cleaned up a bit

--- src/components/main-page/Home.js
+++ src/components/main-page/Home.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>Growing Up in the 90s</h1>
+      <img className='home-img' src="https://gifdb.com/images/high/kawaii-character-enjoys-watching-tv-45k64xai95xcodzj.gif" alt="90s kids" />
     </div>
   );
 };
--- src/components/main-page/Navigation.js
+++ src/components/main-page/Navigation.js
@@ -1,82 +1,9 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Navigation = ({ authenticated }) => {
-//   return (
-//     <nav>
-//       <ul className="nav-links">
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//         {authenticated ? (
-//           <>
-//             <li>
-//               <Link to="/cartoons">Cartoons</Link>
-//             </li>
-//             <li>
-//               <Link to="/movies">Movies</Link>
-//             </li>
-//           </>
-//         ) : (
-//           <li>
-//             <Link to="/login">Login</Link>
-//           </li>
-//         )}
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Navigation;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Navigation = ({ authenticated, onLogout, username }) => {
-//   const handleLogout = () => {
-//     // Call the onLogout function to handle the logout logic in the parent component (App)
-//     onLogout();
-//   };
-
-//   return (
-//     <nav>
-//       <ul className="nav-links">
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//         {authenticated ? (
-//           <>
-//             <li>
-//               <Link to="/cartoons">Cartoons</Link>
-//             </li>
-//             <li>
-//               <Link to="/movies">Movies</Link>
-//             </li>
-//             <li>
-//               <span>Hello, {username}</span>
-//             </li>
-//             <li>
-//               <button onClick={handleLogout}>Logout</button>
-//             </li>
-//           </>
-//         ) : (
-//           <li>
-//             <Link to="/login">Login</Link>
-//           </li>
-//         )}
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Navigation;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = ({ authenticated, onLogout, username }) => {
   const handleLogout = () => {
-    // Call the onLogout function to handle the logout logic in the parent component (App)
+    // Log the user out
     onLogout();
   };
 
@@ -105,7 +32,7 @@ const Navigation = ({ authenticated, onLogout, username }) => {
         {authenticated && (
           <>
             <span>Hello, {username}</span>
-            <button onClick={handleLogout}>Logout</button>
+            <button className='logout-btn' onClick={handleLogout}>Logout</button>
           </>
         )}
       </div>
